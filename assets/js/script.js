57b0a8f965bcf3aad69fc120b8e53331ed8809f7
nah1121 <nahomyeshitla1121@gmail.com>
2023-07-31 22:47:34 +0300

    dd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-const person = {
-    id: 2,
-    firstname: "Abebe",
+const arr = [0,1,2,3,4];
+const len = arr.length
+const z = []
+
+arr.push(5,6)
+n = 0
+for(let i=0; i<len; i++){
+    n += 1
+    z.unshift(arr[n])
 }
-console.log(person.firstname);
+
+
+console.log(z)
